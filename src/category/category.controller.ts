dbfddaa9a51c7537cbfd3a1c48e0ca891929e23c
ashir <ashir@ispgweb.com>
2024-12-30 16:59:50 +0530

    finished category - not tested

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
 import { CreateCategoryDto } from './dto/createCategory.dto';
 import { CategoryService } from './category.service';
@@ -20,6 +20,33 @@ export class CategoryController {
     @ApiOperation({summary:"get all categories"})
     @HttpCode(HttpStatus.ACCEPTED)
     async getAllCategories(){
-        
+        const allCategories = await this.categoryService.getAllCategory()
+        return {message:"success",data:allCategories}
+    }
+
+
+    @Get(':id')
+    @ApiOperation({summary:"get a category details"})
+    @HttpCode(HttpStatus.ACCEPTED)
+    async getSingleCategory(@Param() param){
+        const categoryDetails = await this.categoryService.getSingleCategory(param.id)
+        return categoryDetails
+    }
+
+    @Patch(':id')
+    @ApiOperation({summary:"update a category"})
+    @HttpCode(HttpStatus.ACCEPTED)
+    async updateCategory(@Param() param,@Body() createCategoryDto:CreateCategoryDto){
+        const categoryDetails = await this.categoryService.updateCategory(param.id,createCategoryDto)
+        return categoryDetails
+    }
+
+
+    @Delete(':id')
+    @ApiOperation({summary:"delete a category"})
+    @HttpCode(HttpStatus.ACCEPTED)
+    async deleteCategory(@Param() param){
+        const categoryDetails = await this.categoryService.deleteCategory(param.id)
+        return categoryDetails
     }
 }
